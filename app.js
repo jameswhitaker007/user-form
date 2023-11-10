f856688316caa0d0faf462938927749d07c0b465
James Whitaker <jwhitaker0007@gmail.com>
2023-11-10 06:46:28 -0500

    User.create added

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//Added code
+app.use('/addUser', addUserRouter);
+app.use('/getUser', getUserRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,72 @@
-const alertPlaceholder = $('#alert-placeholder');
+const alertPlaceholder = $("#alert-placeholder");
 
-$('document').ready(function(){
-    $('#form')[0].addEventListener('submit', function(event){
-        console.log('#form');
-        event.preventDefault();
-        $('.wrapper').show();
-        form.classList.add('was-validated');
-        if(!form.checkValidity()) {
-            event.stopPropagation();
-            $('.wrapper').hide();
-            return;
-        }
-        $.post('/addUser', {
+$("document").ready(function () {
+  $("#form")[0].addEventListener("submit", function (event) {
+    console.log("#form");
+    event.preventDefault();
+    $(".wrapper").show();
+    form.classList.add("was-validated");
+    if (!form.checkValidity()) {
+      event.stopPropagation();
+      $(".wrapper").hide();
+      return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: '/addUser',
+        data: {
             user_name: $('#username').val(),
             user_age: $('#age').val(),
             user_city: $('#city').val(),
-            user_phone: $('#phone')
-
-        })
+            user_phone: $('#phone').val(),
+            user_email: $('#email').val()
+        },
+        dataType: "application/json"
     })
-})
+    .done(function(data) {
+        alertPlaceholder.empty();
+        alert("Account created successfully", "success");
+    })
+    .fail(function (error) {
+        console.error(error.responseJSON.message);
+        alertPlaceholder.empty();
+        alert("Account creation failed", "danger");
+    })
+    $(".wrapper").hide();
+
+    /*
+    $.post("/addUser", {
+      user_name: $("#username").val(),
+      user_age: $("#age").val(),
+      user_city: $("#city").val(),
+      user_phone: $("#phone"),
+      user_email: $("#email"),
+    })
+      .done(function (data) {
+        alertPlaceholder.empty();
+        alert("Account created successfully", "success");
+      })
+      .fail(function (error) {
+        console.error(error.responseJSON.message);
+        alertPlaceholder.empty();
+        alert("Account creation failed", "danger");
+      });
+    */
+    
+  });
+});
+
+function alert(message, type) {
+  const wrapper = $("<div></div");
+  wrapper.html(
+    [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      "</div>",
+    ].join("")
+  );
+
+  alertPlaceholder.append(wrapper);
+}
