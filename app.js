8a121d41471babf06ddef1044890132c5c0a11b3
James Whitaker <jwhitaker0007@gmail.com>
2023-11-12 21:58:46 -0500

    favicon.ico route added to app.js

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var favicon = require("serve-favicon");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 //Added code
-const addUserRouter = require('./routes/addUser');
-const getUserRouter = require('./routes/getUser');
-const accountRouter = require('./routes/account');
+const addUserRouter = require("./routes/addUser");
+const getUserRouter = require("./routes/getUser");
+const accountRouter = require("./routes/account");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "favicon.ico")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 //Added code
-app.use('/addUser', addUserRouter);
-app.use('/getUser', getUserRouter);
-app.use('/account', accountRouter);
+app.use("/addUser", addUserRouter);
+app.use("/getUser", getUserRouter);
+app.use("/account", accountRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
